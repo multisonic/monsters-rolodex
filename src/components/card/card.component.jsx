6a82e4d4ed6ab8f5b6f4bae8aa9c4d21aa7249f3
Michael Zaleski <me@mikezaleski.com>
2020-04-10 20:23:14 -0400

    begin work on search-box component

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -6,7 +6,7 @@ export const Card = (props) => (
   <div className='card-containter'>
     <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} />
     <h2> {props.monster.name} </h2>
-    <p> { props.monster.email }</p>
+    <p> {props.monster.email} </p>
   </div>
 )
 
