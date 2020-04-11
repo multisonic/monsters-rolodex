7907f19936dac514628357b7637c7fe39a07f3dc
Michael Zaleski <me@mikezaleski.com>
2020-04-11 07:52:55 -0400

    Updates

---
+++ precache-manifest.971a7b412f0c21ec4d90a06464f68fa0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9e93a7b0d4bd6be271e0daaf6a116d1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "21917394450d5e9acf07",
+    "url": "/monsters-rolodex/static/css/main.ca660cab.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21917394450d5e9acf07",
+    "url": "/monsters-rolodex/static/js/main.7a847b50.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
